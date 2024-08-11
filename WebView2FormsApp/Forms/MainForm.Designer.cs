86fca06088028aa1cb9bccf6bb1785f60a8893fa
Junian Triajianto <author@junian.dev>
2024-08-11 11:26:58 +0700

    Add WebView2

---
+++ WebView2FormsApp/Forms/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WebView2FormsApp.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            webView2Main = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)webView2Main).BeginInit();
+            SuspendLayout();
+            // 
+            // webView2Main
+            // 
+            webView2Main.AllowExternalDrop = true;
+            webView2Main.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            webView2Main.CreationProperties = null;
+            webView2Main.DefaultBackgroundColor = Color.White;
+            webView2Main.Location = new Point(12, 12);
+            webView2Main.Name = "webView2Main";
+            webView2Main.Size = new Size(652, 426);
+            webView2Main.Source = new Uri("https://www.junian.dev/", UriKind.Absolute);
+            webView2Main.TabIndex = 0;
+            webView2Main.ZoomFactor = 1D;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(676, 450);
+            Controls.Add(webView2Main);
+            Name = "MainForm";
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)webView2Main).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2Main;
+    }
+}
--- WebView2FormsApp/Program.cs
+++ WebView2FormsApp/Program.cs
@@ -1,3 +1,5 @@
+using WebView2FormsApp.Forms;
+
 namespace WebView2FormsApp
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace WebView2FormsApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
